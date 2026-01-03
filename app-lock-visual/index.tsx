c4796b79d164eda3107ea572eeaf9a496e868097
lumitechm <support@aqara.my>
2026-01-03 12:40:56 +0800

    Update index.tsx

--- app-lock-visual/index.tsx
+++ app-lock-visual/index.tsx
@@ -1,7 +1,13 @@
-
 import React from 'react';
-import ReactDOM from 'react-domclient';
-import App from '.App';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(React.StrictModeApp React.StrictMode);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
